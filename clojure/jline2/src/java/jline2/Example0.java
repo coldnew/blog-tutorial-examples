38698cd1a46363a280801e255c0becf5d489ab1a
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-09-30 13:06:24 +0800

    jline2: fix building error
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- clojure/jline2/src/java/jline2/Example0.java
+++ clojure/jline2/src/java/jline2/Example0.java
@@ -8,11 +8,21 @@ import jline.console.ConsoleReader;
 public class Example0 {
 
     public static void main(String[] args) {
-        ConsoleReader console = new ConsoleReader();
-        console.setPrompt("java> ");
-        String line = null;
-        while ((line = console.readLine()) != null) {
-            console.println(line);
+        try {
+            ConsoleReader console = new ConsoleReader();
+            console.setPrompt("java> ");
+            String line = null;
+            while ((line = console.readLine()) != null) {
+                console.println(line);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                TerminalFactory.get().restore();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
