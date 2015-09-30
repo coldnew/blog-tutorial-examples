c90a8a750b04797a2ca3e5a2ea899fb356dc2c00
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-09-30 13:40:48 +0800

    lein_with_java: Finish CSVRead example
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- clojure/lein_with_java/project.clj
+++ clojure/lein_with_java/project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.apache.commons/commons-csv "1.2"]]
   :java-source-paths ["src/java"])
---
+++ clojure/lein_with_java/src/java/myapp/CsvRead.java
@@ -0,0 +1,29 @@
+package myapp;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+
+public class CsvRead {
+
+    public static void main(String[] args) {
+        try {
+            CSVParser parser = new CSVParser(new FileReader(args[0]),
+                                             CSVFormat.DEFAULT.withHeader());
+            for (CSVRecord record : parser) {
+                System.out.printf("%s\t%s\n",
+                                  record.get("COL1"),
+                                  record.get("COL2"));
+            }
+            parser.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
