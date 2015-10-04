d0dbc8c488ae0be4da2d724c4a06f90df5e1f420
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-04 09:16:42 +0800

    Example2: clean code
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/cljs_readline/src/readline/example2.cljs
+++ 2015/cljs_readline/src/readline/example2.cljs
@@ -10,30 +10,26 @@
         rl (.createInterface readline
                              (clj->js {:input  (.-stdin  js/process)
                                        :output (.-stdout js/process)}))]
-    ;; mask input char
-    ;; http://stackoverflow.com/questions/24037545/how-to-hide-password-in-the-nodejs-console
     (.on js/process.stdin "data"
-         (fn [char]
-           (.write js/process.stdout "\033[2K") ; clear entire lin
-           (.prompt rl)
-           (.write js/process.stdout
-                   (apply str (repeat (count rl.line) "*")))
-           ))
+         (fn [c]
+           (if  (or (and (>= c \A) (<= c \Z))
+                    (and (>= c \a) (<= c \z))
+                    (and (>= c \0) (<= c \9)))
+             (.write js/process.stdout "\b*"))))
 
     (doto rl
       (.setPrompt "user> ")
       (.on "line"
            (fn [line]
-             (let [line* (str/reverse line)]
-               (case line*
-                 "quit" (.exit js/process) ; real quit application
-                 ;; default
-                 (println (str "You enter: " line*)))
-               (if-not (= line* "quit") (.prompt rl))
-               )))
+             (case line
+               "quit" (.exit js/process) ; real quit application
+               ;; default
+               (println (str "You enter: " line)))
+             (if-not (= line "quit") (.prompt rl))
+             ))
+      (.on "close" #(println "Exit application."))
       (.prompt)
       )))
-
 
 ;; setup node.js starter point
 (set! *main-cli-fn* -main)
