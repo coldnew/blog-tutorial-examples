ef721e2ce09b2ddec9e8bb65df6caaa258f68b1e
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-08 01:29:30 +0800

    qpm-example1: add C++ example
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/qpm-example1/main.cpp
+++ 2015/qpm-example1/main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QDebug>
+
+#include <io/qpm/example/helloqpm.h>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    io::qpm::example::HelloQpm hello;
+    qDebug() << hello.message();
 
     QQmlApplicationEngine engine;
     QPM_INIT(engine) // <-- ADD THIS LINE
