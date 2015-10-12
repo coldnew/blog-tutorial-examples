d04fc79504a4c805225245a75856e2e94ad6ff42
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-10-12 15:43:43 +0800

    add basic template
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ 2015/opencv3_hello/test/opencv3_hello/core_test.clj
@@ -0,0 +1,7 @@
+(ns opencv3-hello.core-test
+  (:require [clojure.test :refer :all]
+            [opencv3-hello.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
