8216844b6932a6314841e0bd00b1bac81689624e
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-11 14:06:13 +0800

    tmp add current
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/renjin_clj/project.clj
+++ 2015/renjin_clj/project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.renjin/renjin-script-engine "0.7.0-RC7"]]
+                 [org.renjin/renjin-script-engine "0.7.0-RC7"]
+                 [incanter "1.9.0"]]
 
   :repositories [["bedatadriven" "https://nexus.bedatadriven.com/content/groups/public/"]])
--- 2015/renjin_clj/src/renjin_clj/core.clj
+++ 2015/renjin_clj/src/renjin_clj/core.clj
@@ -1,8 +1,18 @@
 (ns renjin-clj.core
   (:import [javax.script ScriptEngine ScriptEngineManager ScriptException])
+  (:require [incanter.core :as core]
+            [incanter.charts :as charts]
+            )
   )
 
 (def renjin (.getEngineByName (ScriptEngineManager.) "Renjin"))
 
 (.eval renjin "df <- data.frame(x=1:10, y=(1:10) + rnorm(n=10))")
-(.eval renjin "print(df)")
+(.toString (.eval renjin "print(df)"))
+
+;;org.renjin.sexp.DoubleArrayVector
+;; (extend-type org.renjin.sexp.DoubleArrayVector
+
+;;   )
+
+(vec (.eval renjin "a <- 2; b<- 3; a * b"))
