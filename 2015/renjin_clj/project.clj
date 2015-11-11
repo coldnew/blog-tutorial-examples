856dc4fe3fe24fcf5a2efb066e8dce51c449cd99
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-11 09:52:07 +0800

    Add renjin deps
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/renjin_clj/project.clj
+++ 2015/renjin_clj/project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.renjin/renjin-script-engine "0.7.0-RC7"]]
+
+  :repositories [["bedatadriven" "https://nexus.bedatadriven.com/content/groups/public/"]])
