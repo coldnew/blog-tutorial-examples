fa7c6ed765212c5abedb3537e3a60f7094de77ec
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-18 09:57:39 +0800

    clojure-sqlite-example: fist commit
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

---
+++ 2015/clojure-sqlite-example/test/clojure_sqlite_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-sqlite-example.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-sqlite-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
