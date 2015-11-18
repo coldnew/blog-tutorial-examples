e3acb0db2a60e15e486eff32e0d1d48c8f49aad8
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-18 09:56:43 +0800

    Add current implement
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/csv/project.clj
+++ 2015/csv/project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [incanter "1.5.5"]
+                 [semantic-csv "0.1.0"]
+                 [clojure-csv/clojure-csv "2.0.1"]
+                 ])
--- 2015/csv/src/csv/core.clj
+++ 2015/csv/src/csv/core.clj
@@ -1,6 +1,32 @@
-(ns csv.core)
+(ns csv.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [incanter.io]
+            [incanter.core]
+            )
+  )
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+((fn [k coll]
+   (map #(vector %1 %2) k coll
+        ;;(repeat 4 coll)
+           ))
+#{1 2 3} #{4 5})
+
+
+(:a {:a 1})
+
+
+(with-open [in-file (io/reader (io/resource "test.csv"))]
+  (doall
+   (csv/read-csv in-file)))
+
+(class
+ (incanter.core/to-matrix
+  (incanter.io/read-dataset (io/resource "test.csv")))
+)
+
+(with-open [out-file (io/writer "out-file.csv")]
+  (csv/write-csv out-file
+                 [["abc" "def"]
+                  ["ghi" "jkl"]]))
+;; https://github.com/clojure-cookbook/clojure-cookbook/blob/master/04_local-io/4-20_read-write-csv.asciidoc
