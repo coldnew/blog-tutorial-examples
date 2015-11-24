621bcbcecc77992b5ce9543001e82e67d25b7f51
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-25 03:33:44 +0800

    03: send to gitter
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/mybot/src/mybot/core.clj
+++ 2015/mybot/src/mybot/core.clj
@@ -4,8 +4,22 @@
             [clj-http.client :as client]
             [http.async.client :as http]
             [clojure.data.json :as json]
-            [clojure.core.async :refer [chan go-loop >! <! put!] :as async]
+            [clojure.core.async :refer [chan go-loop >! <! put!] :as async])
   (:gen-class))
+
+(def config {:gitter {:token   "YOUR TOKEN"
+                      :room-id "YOUR ROOM ID"}})
+
+(defn send-to-gitter!
+  "Send message to gitter."
+  [message]
+  (let [{:keys [token room-id]} (:gitter config)]
+    (client/post (str "https://api.gitter.im/v1/rooms/" room-id "/chatMessages")
+                 {:content-type :json
+                  :accept :json
+                  :headers {"Authorization" (str "Bearer " token)}
+                  :conn-timeout (* 10 1000)
+                  :body (json/write-str {:text message})})))
 
 
 (defn -main [& args]
