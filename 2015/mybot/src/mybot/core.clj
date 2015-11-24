cfebca479ecd8f8fa31f49b16a14105693d88746
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-11-25 01:00:08 +0800

    01: Hello, Clojure
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2015/mybot/src/mybot/core.clj
+++ 2015/mybot/src/mybot/core.clj
@@ -1,6 +1,12 @@
-(ns mybot.core)
+(ns mybot.core
+  (:require [irclj.core :as irc]
+            [irclj.events :as events]
+            [clj-http.client :as client]
+            [http.async.client :as http]
+            [clojure.data.json :as json]
+            [clojure.core.async :refer [chan go-loop >! <! put!] :as async]
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn -main [& args]
+  (println "Hello, Clojure!"))
