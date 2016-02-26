b94f1eb79f96b2d96f14934b2a842f2d4bd7dca3
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-02-26 13:44:14 +0800

    twbank-forex: finish example
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- 2016/twbank-forex/project.clj
+++ 2016/twbank-forex/project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hickory "0.6.0"]
+                 [clj-http "2.1.0"]]
+
+  :main twbank-forex.core)
